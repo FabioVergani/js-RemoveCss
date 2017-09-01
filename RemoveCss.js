07a8da5184cb5963f3a77accffbe53f35e29ffb1
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-09-01 19:26:46 +0200

    Update RemoveCss.js

--- RemoveCss.js
+++ RemoveCss.js
@@ -1,36 +1,75 @@
 (function(w){
- function on(a,b){$rem(a,b);$add(a,b)}
- function at(a,b){on(a,function f(x){b(x);$rem(a,f)})}
- function f(evt){
-  const $d=evt.target,$frag=$d.createDocumentFragment(),$dE=$d.documentElement;
-  let e=$frag.appendChild($dE.cloneNode(true)),o=$d.createNodeIterator(e,-1,null);
-  while(e=o.nextNode()){
-   let i=e.nodeType;
-   if(i!==8){
-    if(i===1){
-	 const s=e.tagName.toLowerCase(),x='style';
-	 if(s!==x){
-	  if(s!=='link'){
-	   if(e.hasAttribute(x)){e.removeAttribute(x)}		  
-	  }else{
-		if(e.getAttribute('rel')==='stylesheet'){
-		 e.remove()
-		}
-	  }
-	 }else{
-	  e.remove()
-	 }
-    }
-   }else{
-    e.remove()
-   }
-  };
-  //
-  $dE.replaceWith($frag.firstChild);
- }
- const $=w,$d=$.document,$rem=$.removeEventListener,$add=$.addEventListener;
- console.clear();
- $d.readyState!=='complete'?at('load',f):f({target:$d})
-})(window);
-//========
+ const $=w, $d=$.document, addListener=$.addEventListener, remListener=$.removeEventListener, on=function(a,b){remListener(a,b);addListener(a,b)}, at=function(a,b){on(a,function f(x){b(x);remListener(a,f)})}, atLoad=function(a){$d.readyState!=='complete'?at('load',a):a({target:$d})};
+ atLoad(function(evt){
+  const $d=evt.target, $=$d.defaultView, $dE=$d.documentElement;
+  if($dE!==null){
+   const trace=$.console, css=[], frag=$d.createDocumentFragment(), $dE2=frag.appendChild($dE.cloneNode(true)), walker=$d.createNodeIterator($dE2,-1,null), blankLinePattern=/^\s*[\r\n]/gm;
+   let e=$dE2;
+   trace.group('logs');
+   while(e){
+    let i=e.nodeType,k=false;
+    if(i!==8){
+     if(i!==3){
+      if(i!==1){
+       k=e;//trace.info(i,e);
+      }else{
+       const s=e.tagName.toLowerCase();
+       if(s!=='style'){
+        if(s!=='link'){
+         if(s!=='script'){
+		  if(e.style.display!=='none'){
+			const m=e.attributes;
+			if(m.length>0){
+			 const o=new Set();
+			 if(e.hasAttribute('style')){
+			  const a=e.getAttributeNode('style');
+			  let s=a.value;
+			  if(blankLinePattern.test(s)!==true){
+				a.value=s.trim();  
+				a.ownerNode=e;
+				css.push(a);
+				o.add(a)
+			  };
+			  e.removeAttribute('style')
+			 };
+			 //
+			 while(m.length>0){
+			  let s=m[0].name;
+			  o.add(e.getAttributeNode(s));
+			  e.removeAttribute(s);
+			 };
 
+			 
+			 
+			 e.originalAttributes=o;console.dir(o);
+		   }
+		  }else{
+		   k=e
+		  }		  
+         }else{
+          k=e
+         }
+        }else{
+         if(e.getAttribute('rel')!== 'stylesheet'){
+          //remattrs
+         }else{
+          css.push(k=e)
+         }
+        }
+       }else{
+        css.push(k=e)
+       }
+      }
+     }
+    }else{
+     k=e
+    };
+    if(k!==false){e.remove()};e=walker.nextNode()
+   };
+   $dE.replaceWith($dE2);
+   //   
+   trace.log('cascade:%O',css);
+   trace.groupEnd();
+  }
+ })
+})(window);
